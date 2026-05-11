38d585f2c1bd3f9cb6bf1e957f9b028c28851a19
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-05-11 10:41:51 +0000

    generador de domiov2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
 window.onload = function() {
   //write your code here
+let pronouns = ['the', 'our'];
+let adjectives = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+let extensions = ['.com', '.io', '.net', '.ve', '.es'];
+
+for (let pronombre of pronouns) {
+  for (let adjetivo of adjectives) {
+    for (let sustantivo of nouns) {
+      for (let extension of extensions) {
+        console.log(pronombre + adjetivo + sustantivo + extension);
+      }
+    }
+  }
+}
   console.log("Hello Rigo from the console!");
 };
